b7eff0fb31002f7b4c7ecf57bb711ccfb7c7897f
pilartorr <sauleturme@gmail.com>
2019-01-16 19:25:37 +0100

    structure about done

--- src/App.js
+++ src/App.js
@@ -37,6 +37,50 @@ class App extends Component {
             </div>
           </div>
         </div>
+        <div className="row section-height d-flex align-items-center" id="about">
+         <div className="col col-size-30 m-5">
+          <h1 className="lead text-right ">Hi, It´s me!</h1>
+          <p className="text-right">What I am all about.</p>
+         </div>
+         <div className="col col-size-70 m-5">
+          <p className="text-justify">My name is Pilar Torres. As an artist, I came to Germany. As a web developer,
+             I grow up here. It make me very happy to share my creativity with you and be able
+             to offer to the world of technology a future full of new ideas.
+          </p>
+          <div className="container-progress-box my-5">
+            <div className="progress-box">
+              <p className="mt-3">HTML5, CSS3 & Bootstrap<span className="color-heading pull-right">90%</span></p>
+              <div className="progress">
+                <div className="progress-bar bg-color-base" role="progressbar" data-width="90"></div>
+              </div>
+            </div>
+            <div className="progress-box">
+              <p className="mt-3">JavaSript & jQuery<span className="color-heading pull-right">80%</span></p>
+              <div className="progress">
+                <div className="progress-bar bg-color-base" role="progressbar" data-width="80"></div>
+              </div>
+            </div>
+            <div className="progress-box">
+              <p className="mt-3">React & Redux<span className="color-heading pull-right">75%</span></p>
+              <div className="progress">
+                <div className="progress-bar bg-color-base" role="progressbar" data-width="75"></div>
+              </div>
+            </div>
+            <div className="progress-box">
+              <p className="mt-3">Nodejs, Express & MongoDB<span className="color-heading pull-right">50%</span></p>
+              <div className="progress">
+                <div className="progress-bar bg-color-base" role="progressbar" data-width="50"></div>
+              </div>
+            </div>
+            <div className="progress-box">
+              <p className="mt-3">Photoshop, InDesign & Premiere<span className="color-heading pull-right">85%</span></p>
+              <div className="progress">
+                <div className="progress-bar bg-color-base" role="progressbar" data-width="85"></div>
+              </div>
+            </div>
+          </div>
+         </div>
+        </div>
       </Fragment>
     );
   }
